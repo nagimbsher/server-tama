3ce02f3fc11166573eb150ee54bf00b08b5ac1f4
nagimbsher <49290690+nagimb@users.noreply.github.com>
2025-01-25 09:40:54 +0200

    update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,19 +1,33 @@
-
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// Use the connection string from the .env file
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DB_CONNECTION_STRING,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // This is needed for cloud databases with SSL
   },
 });
 
 module.exports = pool;
+
+
+
+// require('dotenv').config();
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT || 5432,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// module.exports = pool;
 
 
 
