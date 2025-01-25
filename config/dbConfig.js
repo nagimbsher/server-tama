5890a370468bac72f2646b4ae686dfd8d26d354c
nagimbsher <49290690+nagimb@users.noreply.github.com>
2025-01-25 08:02:51 +0200

    Initial commit for server-tama

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,      // PostgreSQL username
-  host: process.env.DB_HOST,      // Database host (e.g., Render or Neon host)
-  database: process.env.DB_NAME,  // Database name
-  password: process.env.DB_PASS,  // PostgreSQL password
-  port: process.env.DB_PORT || 5432, // Default PostgreSQL port
-  ssl: {
-    rejectUnauthorized: false, // Allow SSL connection (required for Neon/Render)
-  },
+  user: process.env.DB_USER, // 'neondb_owner'
+  host: process.env.DB_HOST, // 'ep-holy-poetry-a8c3dad4-pooler.eastus2.azure.neon.tech'
+  database: process.env.DB_NAME, // 'neondb'
+  password: process.env.DB_PASS, // 'npg_lOLa5BrdGDc3'
+  port: process.env.DB_PORT, // 5432
+  ssl: { rejectUnauthorized: false }, // Required for secure connection
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error acquiring client', err.stack);
+  } else {
+    console.log('Connected to the database');
+    release();
+  }
 });
 
 module.exports = pool;
