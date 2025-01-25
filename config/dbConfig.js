2076c0a2d4e830024a9a6de260c5758a72a4fa0b
nagimbsher <49290690+nagimb@users.noreply.github.com>
2025-01-25 08:58:54 +0200

    update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER, // 'neondb_owner'
-  host: process.env.DB_HOST, // 'ep-holy-poetry-a8c3dad4-pooler.eastus2.azure.neon.tech'
-  database: process.env.DB_NAME, // 'neondb'
-  password: process.env.DB_PASS, // 'npg_lOLa5BrdGDc3'
-  port: process.env.DB_PORT, // 5432
-  ssl: { rejectUnauthorized: false }, // Required for secure connection
+  user: process.env.DB_USER, 
+  host: process.env.DB_HOST, 
+  database: process.env.DB_NAME, 
+  password: process.env.DB_PASS, 
+  port: process.env.DB_PORT,
 });
 
 pool.connect((err, client, release) => {
