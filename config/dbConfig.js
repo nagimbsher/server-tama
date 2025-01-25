d15f05508e6adac8f96b4f2d482d7dcd67691e59
nagimbsher <49290690+nagimb@users.noreply.github.com>
2025-01-25 09:03:30 +0200

    update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,43 +1,47 @@
-require('dotenv').config();
 
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER, 
-  host: process.env.DB_HOST, 
-  database: process.env.DB_NAME, 
-  password: process.env.DB_PASS, 
-  port: process.env.DB_PORT,
-});
-
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error('Error acquiring client', err.stack);
-  } else {
-    console.log('Connected to the database');
-    release();
-  }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
 
 
 
-
 // require('dotenv').config();
+
 // const { Pool } = require('pg');
 
 // const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: process.env.DB_PORT || 5432,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   user: process.env.DB_USER, 
+//   host: process.env.DB_HOST, 
+//   database: process.env.DB_NAME, 
+//   password: process.env.DB_PASS, 
+//   port: process.env.DB_PORT,
+// });
+
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     console.error('Error acquiring client', err.stack);
+//   } else {
+//     console.log('Connected to the database');
+//     release();
+//   }
 // });
 
 // module.exports = pool;
+
+
+
+
 
 
