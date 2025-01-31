8de06f8f4f5ff62e86b7fe827434689447976097
nagimbsher <49290690+nagimb@users.noreply.github.com>
2025-01-31 19:19:55 +0200

    updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
 const { Pool } = require('pg');
+require('dotenv').config(); 
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT || 5432,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
