eb5c504d1ca3c7048f4d1264f0055b2be99ac018
nagimbsher <49290690+nagimb@users.noreply.github.com>
2025-02-05 20:48:26 +0200

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Route for registering a user
+
 router.post('/register', authController.registerUser);
 
-// Route for logging in a user
 router.post('/login', authController.loginUser);
 
 
-// Optional GET route for testing purposes (or you can remove it if unnecessary)
+
 router.get('/register', (req, res) => {
   res.send('Registration endpoint');
 });
