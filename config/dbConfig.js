fa95efb3694da1bdf68b17d78a9fe1a16cd6a05e
nagimbsher <49290690+nagimb@users.noreply.github.com>
2025-04-19 20:15:33 +0300

    update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT || 5432,
+  port: process.env.DB_PORT ,
   ssl: { rejectUnauthorized: false }, // Required for Supabase
 });
 
